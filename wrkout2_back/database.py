8062e07802bdf1482ea81e8fa5a524d9bec01282
sqmch <sqmchh@gmail.com>
2022-02-12 08:57:54 +0000

    Fix DATABASE_URL environment variable reference

--- wrkout2_back/database.py
+++ wrkout2_back/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL").replace(
+    "postgres://", "postgresql://", 1
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

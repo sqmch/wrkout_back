35e3d12bde1890b474f73b8c264c6cc0fac83ce0
sqmch <sqmchh@gmail.com>
2022-02-12 03:46:05 +0000

    Misc tweaks

--- wrkout2_back/database.py
+++ wrkout2_back/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://vcexmvkdgoscpm:51c848f37ab1e93745216e1e0434edb872acad6ab9e2b93adb78f691e19ad355@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com/d11koj45j9fsk9"
+SQLALCHEMY_DATABASE_URL = "postgres://vcexmvkdgoscpm:51c848f37ab1e93745216e1e0434edb872acad6ab9e2b93adb78f691e19ad355@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/d11koj45j9fsk9"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
